e723971784bc6cfa37c935e9458e38e531e3df68
meomeoQuan <QUANNADE180924@fpt.edu.vn>
2026-04-21 17:22:07 +0700

    delete be

--- backend-web/Infrastructure/Common/Options/CloudinaryOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Infrastructure.Common.Options;
-
-public class CloudinaryOptions
-{
-    public string CloudName { get; set; } = string.Empty;
-    public string ApiKey { get; set; } = string.Empty;
-    public string ApiSecret { get; set; } = string.Empty;
-}
