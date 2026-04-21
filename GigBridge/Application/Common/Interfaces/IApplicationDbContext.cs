5e1900487752378339af87b51e140196f0470ea3
ductree224 <ductri.18102020@gmail.com>
2026-04-21 17:00:25 +0700

    Add EF Core package and DbSet accessor
    Add Microsoft.EntityFrameworkCore package to the Application project and expose a generic DbSet<TEntity> Set<TEntity>() method on IApplicationDbContext so application code can access EF Core DbSet instances. Added necessary using directives; SaveChangesAsync remains unchanged.

--- GigBridge/Application/Common/Interfaces/IApplicationDbContext.cs
+++ GigBridge/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
 namespace Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
